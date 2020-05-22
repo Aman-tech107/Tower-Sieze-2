d53c0ac90f31d58246a5891d503702a0f9f03720
Aman-tech107 <karan.mukesh1985@gmail.com>
2020-05-22 18:55:57 +0530

    Second Sling Shot

--- Stand.js
+++ Stand.js
@@ -2,7 +2,8 @@ class Stand {
     constructor(x, y, width, height){
         var options = {
             'isStatic': true
-body = Bodies.rectangle(x, y, width, height, options);
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
         this.height = height;
         World.add(world, this.body);
